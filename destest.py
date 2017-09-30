7b6914962e46d41602c2d3f21ff966e42bc190c5
minamibashi rearn <rearn499+git@gmail.com>
2017-09-30 15:03:26 +0900

    Add destest

---
+++ destest.py
@@ -0,0 +1,12 @@
+from Crypto.Cipher import DES
+
+# "abcdefgh" がキーになる(キーは8バイトの長さでなければならない)
+obj = DES.new("abcdefgh", DES.MODE_ECB)
+plain = "Guido van Rossum is a space alien."
+# 文字数は8の倍数でなければエラーになるのでplainに６文字結合する
+ciph = obj.encrypt(plain + "XXXXXX")
+decrypt = obj.decrypt(ciph)
+print "plain  :", plain
+print "encrypt:", ciph
+print "decrypt:", decrypt
+assert plain = decrypt, "error"
