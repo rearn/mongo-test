47aab5075b2221693eded256dabadb10f2fa2093
minamibashi rearn <rearn499+git@gmail.com>
2017-09-30 15:21:46 +0900

    lsの修正

--- destest.py
+++ destest.py
@@ -3,11 +3,10 @@ from binascii import hexlify
 
 # "abcdefgh" がキーになる(キーは8バイトの長さでなければならない)
 obj = DES.new("abcdefgh", DES.MODE_ECB)
-plain = "Guido van Rossum is a space alien."
+plain = b"Guido van Rossum is a space alien."
 # 文字数は8の倍数でなければエラーになるのでplainに６文字結合する
-ciph = obj.encrypt(plain + "XXXXXX")
+ciph = obj.encrypt(plain + b"XXXXXX")
 decrypt = obj.decrypt(ciph)
-print("plain  :" + plain)
+print("plain  :" + plain.decode('utf-8'))
 print("encrypt:" + hexlify(ciph).decode('utf-8'))
 print("decrypt:" + decrypt.decode('utf-8'))
-#assert plain == decrypt, "error"
