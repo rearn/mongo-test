4090fa219f67f87c8d160821f62c9c1707d9c8e6
minamibashi rearn <rearn499+git@gmail.com>
2017-10-02 00:07:19 +0900

    テスト１８

--- destest.py
+++ destest.py
@@ -22,9 +22,10 @@ db = client.my_database
 co = db.my_collection
 ca = db.counters
 ca.insert_one({"id": "user_id", 'seq': 0})
-ret = ca.update({'id': 'user_id'}, {'$inc': { 'seq': 1 }})
-# ret = db.command('findandmodify', 'counters', {'query': {'id': 'user_id'},'update': {$inc: { 'seq': 1 }, 'upsert': True}, 'new': True})
-print(ret)
+for i in [1, 2, 3, 4]:
+    print(i)
+    ret = ca.update({'id': 'user_id'}, {'$inc': { 'seq': 1 }})
+    print(ret)
 #ca.findAndModify({"_id": "user_id"}, , upsert = True)
 co.insert_one({"num": a, "encrypt": cipj_u, "decrypt": decrypt_u})
 for data in co.find():
