fbd060bb4a50d34725e3c495e07ec0d8b1133718
minamibashi rearn <rearn499+git@gmail.com>
2017-10-01 23:38:58 +0900

    テスト１１

--- destest.py
+++ destest.py
@@ -20,7 +20,7 @@ print("decrypt:" + decrypt_u)
 client = pymongo.MongoClient('localhost', 27017)
 db = client.my_database
 co = db.my_collection
-ret = db.command('findandmodify', 'counters', {'query': {'id': 'user_id'},'update': {'$inc': { 'seq': 1 }, 'upsert': True}, 'new': True})
+ret = db.command('findandmodify', 'counters', {'query': {'id': 'user_id'},'update': {$inc: { 'seq': 1 }, 'upsert': True}, 'new': True})
 #ca.findAndModify({"_id": "user_id"}, , upsert = True)
 co.insert_one({"num": a, "encrypt": cipj_u, "decrypt": decrypt_u})
 for data in co.find():
