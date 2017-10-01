cee9929e0d69f4142d51771d06e3dc82f384c731
minamibashi rearn <rearn499+git@gmail.com>
2017-10-01 19:59:39 +0900

    てすと

--- destest.py
+++ destest.py
@@ -10,12 +10,17 @@ plain = a.to_bytes(8, 'big')
 # 文字数は8の倍数でなければエラーになるのでplainに６文字結合する
 ciph = obj.encrypt(plain)
 decrypt = obj.decrypt(ciph)
-print("plain  :" + hexlify(plain).decode('utf-8'))
-print("encrypt:" + hexlify(ciph).decode('utf-8'))
-print("decrypt:" + hexlify(decrypt).decode('utf-8'))
+
+plain_u   = hexlify(plain).decode('utf-8')
+cipj_u    = hexlify(ciph).decode('utf-8')
+decrypt_u = hexlify(decrypt).decode('utf-8')
+
+print("plain  :" + plain_u)
+print("encrypt:" + cipj_u)
+print("decrypt:" + decrypt_u)
 client = pymongo.MongoClient('localhost', 27017)
 db = client.my_database
 co = db.my_collection
-co.insert_one(json.dumps({"encrypt": ciph, "decrypt": decrypt}))
+co.insert_one(json.dumps({"num": a, "encrypt": cipj_u, "decrypt": decrypt_u}))
 for data in co.find():
     print(data)
