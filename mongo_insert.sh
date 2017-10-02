312fc0bf7d3c479f5a99d7772509973333320f7e
minamibashi rearn <rearn499+git@gmail.com>
2017-10-03 00:19:24 +0900

    テスト２４

--- mongo_insert.sh
+++ mongo_insert.sh
@@ -1,1 +1,2 @@
-#mongo --eval 'db.counters.insert({"id": "user_id", seq: 0});'
+mongo --eval 'use my_database; db.counters.insert({"id": "user_id", seq: 0});'
+mongo --eval 'use my_database; db.counters.find();'
