eeed54f9d453b4d29ca6632755ff98a490edc17d
minamibashi rearn <rearn499+git@gmail.com>
2017-10-09 00:23:45 +0900

    テスト３６

--- mongo_insert.sh
+++ mongo_insert.sh
@@ -1,3 +1,3 @@
 mongo my_database --eval 'db.createUser({user:"test", pwd:"passwd", roles: [{role:"readWrite", db:"my_database"}]});'
-mongo my_database --eval 'db.my_database.counters.insert({"id": "user_id", seq: 0});'
-mongo my_database --eval 'db.my_database.counters.find();'
+mongo my_database --eval 'use my_database; db.counters.insert({"id": "user_id", seq: 0});'
+mongo my_database --eval 'use my_database; db.counters.find();'
