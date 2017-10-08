192fe0e169533d41d332d994d9e703f6f35fb37e
minamibashi rearn <rearn499+git@gmail.com>
2017-10-09 01:05:32 +0900

    テスト４３

--- mongo_insert.py
+++ mongo_insert.py
@@ -4,4 +4,5 @@ client = pymongo.MongoClient('localhost', 27017)
 db = client.database
 db.add_user('test', 'passwd', roles=[{'role': "readWrite", 'db': "database"}])
 db.counters.insert_one({"id": "user_id", 'seq': 0})
-print(db.counters.find())
+for data in db.counters.find():
+    print(data)
