c0e30ee6134c025d637b8a0da6edc98309d4d04d
minamibashi rearn <rearn499+git@gmail.com>
2017-10-24 22:49:01 +0900

    Revert "テスト４４"
    This reverts commit 6f5f363a8bf64b29e906d828984f9d2e0703785e.

--- destest.py
+++ destest.py
@@ -9,17 +9,19 @@ print(plain_u)
 print(cipj_u)
 print(decrypt_u)
 # client = pymongo.MongoClient('localhost', 27017, username='test', password='passwd')
-# client = pymongo.MongoClient('localhost', 27017)
-client = pymongo.MongoClient('mongodb://%s:%s@localhost' % ('test', 'passwd'))
+client = pymongo.MongoClient('localhost', 27017)
 db = client.database
 db.authenticate('test', 'passwd')
 co = db.collection
 ca = db.counters
+#ca.insert_one({"id": "user_id", 'seq': 0})
 for i in [1, 2, 3, 4]:
     print(i)
-    ret = ca.find_and_modify({'id': 'user_id'}, {'$inc': {'seq': 1}})
+    ret = ca.find_and_modify({'id': 'user_id'}, {'$inc': { 'seq': 1 }})
     print(ret)
-co.insert_one({"num": 255, "encrypt": cipj_u, "decrypt": decrypt_u})
+# ret = db.command('findandmodify', 'counters', {'query': {'id': 'user_id'},'update': {$inc: { 'seq': 1 }, 'upsert': True}, 'new': True})
+#ca.findAndModify({"_id": "user_id"}, , upsert = True)
+co.insert_one({"num": a, "encrypt": cipj_u, "decrypt": decrypt_u})
 for data in co.find():
     print(data)
 
