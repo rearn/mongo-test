d9294ece69645a5ff7b8f753b8c18aa3e35be38d
minamibashi rearn <rearn499+git@gmail.com>
2017-10-24 22:53:20 +0900

    テスト４６

--- destest.py
+++ destest.py
@@ -1,12 +1,11 @@
 from Crypto.Cipher import DES
-import pymongo
-import json
 from binascii import hexlify
+import base64
 
 # "abcdefgh" がキーになる(キーは8バイトの長さでなければならない)
-obj = DES.new(b'abcdefgh', DES.MODE_ECB)
-a = 255
-plain = a.to_bytes(8, 'big')
+obj = DES.new(b'12345678', DES.MODE_ECB)
+a = b'1234567890a='
+plain = base64.urlsafe_b64decode(a)
 ciph = obj.encrypt(plain)
 decrypt = obj.decrypt(ciph)
 
@@ -18,22 +17,3 @@ print("plain  :" + plain_u)
 print("encrypt:" + cipj_u)
 print(ciph)
 print("decrypt:" + decrypt_u)
-client = pymongo.MongoClient('localhost', 27017, username='test', password='passwd')
-# client = pymongo.MongoClient('localhost', 27017)
-db = client.database
-db.authenticate('test', 'passwd')
-co = db.collection
-ca = db.counters
-#ca.insert_one({"id": "user_id", 'seq': 0})
-for i in [1, 2, 3, 4]:
-    print(i)
-    ret = ca.find_and_modify({'id': 'user_id'}, {'$inc': { 'seq': 1 }})
-    print(ret)
-# ret = db.command('findandmodify', 'counters', {'query': {'id': 'user_id'},'update': {$inc: { 'seq': 1 }, 'upsert': True}, 'new': True})
-#ca.findAndModify({"_id": "user_id"}, , upsert = True)
-co.insert_one({"num": a, "encrypt": cipj_u, "decrypt": decrypt_u})
-for data in co.find():
-    print(data)
-
-for data in ca.find():
-    print(data)
